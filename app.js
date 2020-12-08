8afc9b430f004cf0668bb46ee427c3cad3b364b0
Jlrajkumar <jlrkumarjaddu@gmail.com>
2020-12-08 05:05:39 +0000

    To broadcast data a handler is created.

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || 3000;
 
 app.use("/", express.static(__dirname + "/public") );
 
+
+// Socket connection and emit data on drawing channel
+const onConnection = socket => {
+socket.on("drawing", data =>socket.broadcast.emit("drawing",data));
+};
+//when connection event happens , call onConnection listener
+io.on("connection", onConnection )
+
 http.listen(port, () => {
  console.log("Listening at port " + port + " and server started.");
 
