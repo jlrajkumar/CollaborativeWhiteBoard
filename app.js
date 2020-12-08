7d68098b23910b72a118dab09c8bb41a136151a7
Jlrajkumar <jlrkumarjaddu@gmail.com>
2020-12-08 04:49:40 +0000

    added http io server and updated app.listen to http.listen

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const http =require("http").Server(app); // from express package
+const io = require("socket.io")(http);  //bounds to http server
+
 const port = process.env.PORT || 3000;
 
 
 app.use("/", express.static(__dirname + "/public") );
 
-app.listen(port, () => {
+http.listen(port, () => {
  console.log("Listening at port " + port + " and server started.");
 
 }  );
